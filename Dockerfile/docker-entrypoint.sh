8e31540a2dd4638d10156200486a2644370083e6
Julayv <jul.y@orange.fr>
2018-02-24 00:46:18 +0100

    Resolving issues on auto-update Suricata rules

--- Dockerfile/docker-entrypoint.sh
+++ Dockerfile/docker-entrypoint.sh
@@ -3,8 +3,11 @@
 # Update suricata rules
 suricata-update
 
-# Making an auto-update every 12 hours
-(/usr/bin/crontab -l 2>/dev/null; echo "0 12 * * * /etc/suricata/suricata-update.sh") | /usr/bin/crontab -
+# Start cron
+crond
+
+# Add cronjob
+crontab /etc/cron.d/suricata-update-cron
 
 # Started suricata
 /usr/bin/suricata -c /etc/suricata/suricata.yaml -q 0
